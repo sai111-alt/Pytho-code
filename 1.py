d126e5101aac3e999457e5183a28d855c5440165
sai111-alt <78285309+sai111-alt@users.noreply.github.com>
2024-03-02 22:39:37 +0800

    C Practice

---
+++ 1.py
@@ -0,0 +1,10 @@
+print('hello world')
+# print(value, ..., sep=' ', end='ln')
+# 1.这里的value如果输入一个或多个，则打印一个或多个
+# 2.sep 代表可自定义value之间的符号，默认为空值
+# 3.end代表可自定义value结尾的符号，默认为换行符号
+print('hello', 'world')
+print('hello', 'world',sep=',')
+print('hello', 'world',\
+      sep=',',end='.')
+
