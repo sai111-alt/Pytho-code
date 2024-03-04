e6cda601ddf0616b421fb9ecb5485ddcae6b737b
sai111-alt <78285309+sai111-alt@users.noreply.github.com>
2024-03-04 23:24:55 +0800

    Study

--- 1.py
+++ 1.py
@@ -3,8 +3,15 @@ print('hello world')
 # 1.这里的value如果输入一个或多个，则打印一个或多个
 # 2.sep 代表可自定义value之间的符号，默认为空值
 # 3.end代表可自定义value结尾的符号，默认为换行符号
+'''
+多行注释
+'''
 print('hello', 'world')
-print('hello', 'world',sep=',')
-print('hello', 'world',\
-      sep=',',end='.')
+print('hello', 'world', sep=',')
+print('hello', 'world', \
+      sep=',', end='.')
 
+# python是以缩进来包含的，而不是大括号
+# 格式化快捷键设置为：ctrl+l
+for i in range(10):
+    print(i)
